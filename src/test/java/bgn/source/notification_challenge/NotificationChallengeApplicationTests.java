4b81f5e61d9684aab7ee9735894ae64e17477c2a
bgnoatto <bgnoatto@gmail.com>
2026-04-27 09:26:55 -0300

    feat: first commit

---
+++ src/test/java/bgn/source/notification_challenge/NotificationChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package bgn.source.notification_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
