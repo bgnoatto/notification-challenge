7543deaf2f78df226d36d16f67906e75c7ea26ae
bgnoatto <bgnoatto@gmail.com>
2026-04-28 19:52:02 -0300

    fix: rename package name

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # notification-challenge
+
+## Code Conventions
+
+### Do not use Lombok on JPA entities
+
+`@Data` generates `equals()` and `hashCode()` based on all fields. On JPA entities this is an anti-pattern: the `id` is `null` before persisting, which means `hashCode` changes after saving. This breaks collections like `HashSet` or `HashMap`.
+
+Use explicit getters and setters, formatted in multiple lines.
--- src/main/java/bgn/source/notification/model/User.java
+++ src/main/java/bgn/source/notification/model/User.java
@@ -1,6 +1,11 @@
 package bgn.source.notification.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
@@ -22,18 +27,43 @@ public class User {
     @Column(name = "id_pokemons", columnDefinition = "integer[]")
     private Integer[] idPokemons;
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public Long getId() {
+        return id;
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getName() {
+        return name;
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public Integer[] getIdPokemons() { return idPokemons; }
-    public void setIdPokemons(Integer[] idPokemons) { this.idPokemons = idPokemons; }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer[] getIdPokemons() {
+        return idPokemons;
+    }
+
+    public void setIdPokemons(Integer[] idPokemons) {
+        this.idPokemons = idPokemons;
+    }
 }
